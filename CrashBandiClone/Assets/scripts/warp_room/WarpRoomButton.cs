dbb368ef69b83285b12accefc14f8f5df431cc53
Jean-Baptiste Fleury <27734857+JeanBaptisteFleury@users.noreply.github.com>
2021-02-16 17:25:20 +0100

    WarpRoom almost done

---
+++ CrashBandiClone/Assets/scripts/warp_room/WarpRoomButton.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarpRoomButton : MonoBehaviour
+{
+    private Renderer renderer;
+    public short Level = 0;
+    public Texture normal;
+    public Texture pressed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Touch");
+        renderer.material.mainTexture = pressed;
+        GameObject.Find("portal").GetComponent<WarpRoomPortal>().setLevel(Level);
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        renderer.material.mainTexture = normal;
+    }
+}
