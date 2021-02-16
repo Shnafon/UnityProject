a81c4d03de979fdfeb39233b1c2bda9d34cc1df4
pierre.monte <pierre.monte@epitech.eu>
2021-02-16 14:43:50 +0100

    add Simple UI ingame

---
+++ CrashBandiClone/Assets/Rotate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 10, 0) * Time.deltaTime);
+    }
+}
