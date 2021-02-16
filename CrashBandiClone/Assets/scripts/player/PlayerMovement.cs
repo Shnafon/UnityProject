f9988122bea9bc70d6a05c7afe0afc109a1ebbe3
Jean-Baptiste Fleury <27734857+JeanBaptisteFleury@users.noreply.github.com>
2021-02-16 15:15:04 +0100

    Basic player movement and tps camera working

---
+++ CrashBandiClone/Assets/scripts/player/PlayerMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * speed);
+
+    }
+}
