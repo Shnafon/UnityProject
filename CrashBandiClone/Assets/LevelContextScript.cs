c75d5715b2e5eddf275772ef5830641ad46d3d44
pierre.monte <pierre.monte@epitech.eu>
2021-03-17 15:32:59 +0100

    modify scene + add collision on trap + physics on all stuff

--- CrashBandiClone/Assets/LevelContextScript.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LevelContextScript : MonoBehaviour
-{
-    public int nb_WampasFruit = 0;
-}
--- CrashBandiClone/Assets/scripts/warp_room/WarpRoomPortal.cs
+++ CrashBandiClone/Assets/scripts/warp_room/WarpRoomPortal.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class WarpRoomPortal : MonoBehaviour
 {
-    private short level = 0;
+    //private short level = 0;
     // Start is called before the first frame update
     void Start()
     {
