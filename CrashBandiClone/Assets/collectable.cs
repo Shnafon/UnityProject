1c2896bae2e7bd817445ee1db5c4caee1f7a43be
pierre.monte <pierre.monte@epitech.eu>
2021-03-24 15:50:12 +0100

    add some work

---
+++ CrashBandiClone/Assets/collectable.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class collectable : MonoBehaviour
+{
+    public GameObject currentCandy;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Text fruittext =  currentCandy.GetComponent<Text>();
+        int actualfruit = System.Convert.ToInt32(fruittext.text);
+        fruittext.text = System.Convert.ToString(actualfruit + 1);
+        Debug.Log("Wumpa Wumpa collectable");
+        Destroy(this.gameObject);
+    }
+}
