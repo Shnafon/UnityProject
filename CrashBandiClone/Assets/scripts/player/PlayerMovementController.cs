6cb2024e98b74e76c236e45aa9681a02b4dba9a8
pierre.monte <pierre.monte@epitech.eu>
2021-03-30 18:37:35 +0200

    bind animation to Player Controller

--- CrashBandiClone/Assets/scripts/player/PlayerMovementController.cs
+++ CrashBandiClone/Assets/scripts/player/PlayerMovementController.cs
@@ -10,6 +10,7 @@ public class PlayerMovementController : MonoBehaviour
     public float rotateSpeed = 3.0F;
     private Vector3 moveDirection = Vector3.zero;
     public CharacterController controller;
+    public GameObject player;
 
     // Update is called once per frame
     void Update()
@@ -18,11 +19,27 @@ public class PlayerMovementController : MonoBehaviour
         
         if (controller.isGrounded)
         {
+            player.GetComponent<Animator>().SetBool("Jumping", false);
             moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
+            if (moveDirection == Vector3.zero)
+            {
+                Debug.Log("not moving");
+                player.GetComponent<Animator>().SetBool("Running", false);
+            }
+            else
+            {
+                Debug.Log("moving");
+                player.GetComponent<Animator>().SetBool("Running", true);
+            }
             if (Input.GetButton("Jump"))
+            {
                 moveDirection.y = jumpSpeed;
+                Debug.Log("Jump");
+                player.GetComponent<Animator>().SetBool("Running", false);
+                player.GetComponent<Animator>().SetBool("Jumping", true);
+            }
         }
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
