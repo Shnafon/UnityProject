026570d1e84c24792c9550bf63d8af9d9b2dc0b3
Jean-Baptiste Fleury <27734857+JeanBaptisteFleury@users.noreply.github.com>
2021-04-05 18:16:31 +0200

    Added a Pause menu

---
+++ CrashBandiClone/Assets/scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private GameObject[] pauseUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        pauseUI = GameObject.FindGameObjectsWithTag("pauseUI");
+        foreach (GameObject g in pauseUI)
+        {
+            g.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                showPaused();
+                Time.timeScale = 0;
+            }
+            else
+            {
+                hidePaused();
+                Time.timeScale = 1;
+            }
+        }
+    }
+
+    public void showPaused()
+    {
+        foreach(GameObject g in pauseUI)
+        {
+            g.SetActive(true);
+        }
+    }
+
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseUI)
+        {
+            g.SetActive(false);
+        }
+    }
+}
